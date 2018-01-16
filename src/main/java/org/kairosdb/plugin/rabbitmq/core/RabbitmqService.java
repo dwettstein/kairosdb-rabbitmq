334c3fdaf54d1800e552ed495fa6bdd3b73842cf
David Wettstein <dwettstein@gmail.com>
2018-01-16 20:52:09 +0100

    Enable RabbitMQ automaticRecovery

--- src/main/java/org/kairosdb/plugin/rabbitmq/core/RabbitmqService.java
+++ src/main/java/org/kairosdb/plugin/rabbitmq/core/RabbitmqService.java
@@ -194,6 +194,7 @@ public class RabbitmqService
             rabbitmqConnectionFactory.setRequestedChannelMax(rabbitmqChannelMax);
             rabbitmqConnectionFactory.setRequestedFrameMax(rabbitmqFrameMax);
             rabbitmqConnectionFactory.setRequestedHeartbeat(rabbitmqHearbeat);
+            rabbitmqConnectionFactory.setAutomaticRecoveryEnabled(true);
 
             // Get KairosDatastore implementation
             KairosDatastore kairosDatabase = googleInjector.getInstance(KairosDatastore.class);
